5488e31f1cd38e7e702bdc86067305dd822458d6
ivan Jankovic <jankoemail@gmail.com>
2020-04-22 15:25:04 -0400

    Commented out assert False

--- test_main.py
+++ test_main.py
@@ -23,14 +23,14 @@ def client():
 
 
 def test_health(client):
-    assert False
+    # assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
 
 def test_auth(client):
-    assert False
+    # assert False
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
