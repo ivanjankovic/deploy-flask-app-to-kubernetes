7815e2da7f065a2fb1e6ffc3a20856d1e408d1a2
ivan Jankovic <jankoemail@gmail.com>
2020-04-22 15:14:34 -0400

    Adding Tests to the Build

--- test_main.py
+++ test_main.py
@@ -23,17 +23,19 @@ def client():
 
 
 def test_health(client):
+    assert False
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
 
 def test_auth(client):
+    assert False
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
-                           data=json.dumps(body),
-                           content_type='application/json')
+                        data=json.dumps(body),
+                        content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
